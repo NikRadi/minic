7b10dd03157206f7f6fab23eab9eb008bdbd9275
Nik Radi <nik.radi@live.dk>
2025-01-26 14:50:56 +0100

    add test to define directive

--- tests/Define.c
+++ tests/Define.c
@@ -2,8 +2,10 @@
 #define MY_VAR2 20
 #define MY_VAR3 (1 + 2)
 #define PLUS +
+#define PRINTF printf("%d\n"
 
 int main() {
     printf("%d %d %d\n", MY_VAR1, MY_VAR2, MY_VAR1 + MY_VAR2);
     printf("%d %d\n", MY_VAR3, MY_VAR3 PLUS MY_VAR3);
+    PRINTF, MY_VAR1);
 }
